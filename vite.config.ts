a8a0b3af2fc07245f427532892fd1d78b51a4923
mikhailusachev <mikhailusachev@users.noreply.github.com>
2023-01-19 13:56:40 +0300

    i suck at ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { UserConfigFn } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 import { overrideVaadinConfig } from './vite.generated';
 
-const customConfig: UserConfigFn = (env) => ({
-  // Here you can add custom Vite parameters
-  // https://vitejs.dev/config/
-});
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  }
+})
 
-export default overrideVaadinConfig(customConfig);
+export default overrideVaadinConfig(defineConfig);
